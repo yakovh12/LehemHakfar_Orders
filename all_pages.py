173af19b6b39ba6664acffb6bb71f1e7f9bce10b
yakov.hazout <yakov.hazout@finaloop.com>
2025-01-11 09:00:21 +0200

    ready to upload

---
+++ all_pages.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import os
+
+# Set up the main page
+st.set_page_config(page_title="Order Application", layout="centered")
+
+st.title("Welcome to the Order Application")
+
+
