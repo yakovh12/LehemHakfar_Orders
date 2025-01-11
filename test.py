12b2c62f3cb8233fa4c3b272c69a02bf838e68b3
yakov.hazout <yakov.hazout@finaloop.com>
2025-01-11 18:52:58 +0200

    ready to upload

--- test.py
+++
@@ -1,9 +0,0 @@
-import streamlit_authenticator as stauth
-
-# List of plaintext passwords
-passwords = ['password1', 'password2', 'password3']
-
-# Generate hashed passwords
-hashed_passwords = stauth.Hasher(passwords).generate()
-
-print(hashed_passwords)
